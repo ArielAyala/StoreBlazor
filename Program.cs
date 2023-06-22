030162c0dbf2f87a3b80a319849faaf8f0b5a040
Ariel Ayala <arielayalamail@gmail.com>
2023-06-22 15:07:38 -0400

    Put api URL in appsettings.json and configure in program.cs

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@ using StoreBlazor;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var apiURL = builder.Configuration.GetValue<string>("apiURL");
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiURL) });
 
 await builder.Build().RunAsync();
