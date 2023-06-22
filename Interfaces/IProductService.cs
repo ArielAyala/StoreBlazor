28dfffe98b4b4815ee24afe898415bca30a6de8a
Ariel Ayala <arielayalamail@gmail.com>
2023-06-22 16:30:57 -0400

    Add product and delete product

--- Interfaces/IProductService.cs
+++ Interfaces/IProductService.cs
@@ -3,4 +3,6 @@ namespace StoreBlazor;
 public interface IProductService
 {
     Task<List<Product>?> GetProducts();
+    Task AddProduct(Product product);
+    Task DeleteProduct(int idProduct);
 }
--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -1,9 +1,10 @@
+using System.Net.Http.Json;
 using System.Text.Json;
 
 
 namespace StoreBlazor;
 
-public class ProductService: IProductService
+public class ProductService : IProductService
 {
     public readonly HttpClient _client;
     private readonly JsonSerializerOptions _options;
@@ -23,5 +24,26 @@ public class ProductService: IProductService
             throw new ApplicationException(content);
         }
         return JsonSerializer.Deserialize<List<Product>>(content, _options);
+    }
+
+    public async Task AddProduct(Product product)
+    {
+
+        var response = await _client.PostAsync("/v1/products", JsonContent.Create(product));
+        var content = await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new ApplicationException(content);
+        }
+    }
+
+    public async Task DeleteProduct(int productId)
+    {
+        var response = await _client.DeleteAsync($"/v1/products/{productId}");
+        var content = await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new ApplicationException(content);
+        }
     }
 }
