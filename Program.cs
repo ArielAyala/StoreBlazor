57d813667b8cd98206ac0f8fa7aa827b23a699e3
Ariel Ayala <arielayalamail@gmail.com>
2023-07-06 21:28:41 -0400

    Install and config Toast

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Blazored.Toast;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using StoreBlazor;
@@ -6,6 +7,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 var apiURL = builder.Configuration.GetValue<string>("apiURL");
+
+builder.Services.AddBlazoredToast();
+
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiURL) });
